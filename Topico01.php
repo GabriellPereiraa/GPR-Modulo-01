c1ab71cbe1c5d4d7f1a0357462220533eb29c1b7
Gabriel Pereira <78962152+GabriellPereiraa@users.noreply.github.com>
2021-08-03 15:30:50 -0300

    start

---
+++ Topico01.php
@@ -0,0 +1,195 @@
+<?php 
+    // HEADER
+    include('layout/header.php');
+?>
+<body>
+    <!-- PLUGIN DO FADE IN -->
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script>
+        AOS.init();
+    </script>
+    
+    <!-- MENU -->
+    <?php include('layout/menu.php') ?>
+        <div id="Inicio"></div>
+        <div id="main">
+            <!-- NAV TOP -->
+            <?php include('layout/nav.php') ?>
+            <!-- CONTEUDO INICIO -->
+
+
+                <div class="main-content container-fluid Texto">
+                    <div class="row"> <!-- Begin row -->
+                        <div class="col-md-1"></div>
+                        <div class="col-md-10"> <!-- Begin col-md-10 -->
+                                <div class="page-title">
+                                
+                                <h2 class="Titulo" >Aula 1 Políticas Públicas:  origens e conceitos </h2>
+                                <br>
+
+                                <p class="Texto">Antes de entrarmos diretamente nos conceitos, façamos uma pequena introdução esclarecendo algumas relações e estabelecendo diferenciações entre temas recorrentes quando se fala de política pública.</p>
+
+                                <p class="SubSubTitulo">Pois bem, o que entendemos pelo termo sociedade?</p>
+
+                                <div class="bd-callout bd-callout-success">
+                                    <p class="Texto">
+                                    <em>"Sociedade é um conjunto de indivíduos, dotados de interesses e recursos de poder diferenciados, que interagem continuamente a fim de satisfazer às suas necessidades. As sociedades complexas são caracterizadas pela diferenciação social, o que permite a coexistência de situações de cooperação, competição, conflito e confronto."</em>
+                                    </p>
+
+                                    <p>– Rua, 2014</p>
+                                </div>
+                                <br>
+
+                                <p class="Texto">A existência dos conflitos sociais requer, para a preservação da vida em sociedade, que os indivíduos construam regras, limites, princípios, normas, valores e objetivos que sejam o mais consensual possível. Entretanto, o consenso não acontece naturalmente, precisa ser construído.</p>
+
+                                <div class="card text-black border-success Texto">
+                                        <div class="card-body">
+                                            <p class="card-text">Quando os conflitos surgem, é possível o uso de duas forças para administrá-los: a coerção e a política. A primeira custa caro e causa desgaste para quem a usa, já a segunda contribui mais efetivamente na construção de consensos e no controle dos conflitos.</p>
+                                        </div>
+                                </div>
+                                <br>
+
+                                <p class="Texto">Mas quando falamos em política, estamos nos referindo a um campo vasto do qual se ocupa a Ciência Política. Segundo Maar (1994), o termo “política” foi cunhado a partir da atividade social desenvolvida pelos homens da pólis, a “cidade-Estado” grega. </p>
+
+                                <div class="card text-black border-success Texto">
+                                        <div class="card-body">
+                                            <p class="card-text">O filósofo grego Aristóteles, em sua obra Ética a Nicômaco, já afirmava que a política utiliza as demais ciências e, por outro lado, legisla sobre o que devemos e o que não devemos fazer, a finalidade dessa ciência deve abranger as das outras, de modo que essa finalidade será o bem humano.</p>
+                                        </div>
+                                </div>
+                                <br>
+
+                                <p class="Texto">Então, comecemos deixando claro que política não é a mesma coisa que política pública, como nos mostra os quadros a seguir:</p>
+
+                                <div class="accordion accordion-flush" id="AccordionFase01">
+                                <div class="text-center">
+                                    <h6 class="fs-6 SmallCaps badge bg-warning text-dark"><svg class="svg-inline--fa fa-lightbulb fa-w-11" aria-hidden="true" focusable="false" data-prefix="fa" data-icon="lightbulb" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512" data-fa-i2svg=""><path fill="currentColor" d="M96.06 454.35c.01 6.29 1.87 12.45 5.36 17.69l17.09 25.69a31.99 31.99 0 0 0 26.64 14.28h61.71a31.99 31.99 0 0 0 26.64-14.28l17.09-25.69a31.989 31.989 0 0 0 5.36-17.69l.04-38.35H96.01l.05 38.35zM0 176c0 44.37 16.45 84.85 43.56 115.78 16.52 18.85 42.36 58.23 52.21 91.45.04.26.07.52.11.78h160.24c.04-.26.07-.51.11-.78 9.85-33.22 35.69-72.6 52.21-91.45C335.55 260.85 352 220.37 352 176 352 78.61 272.91-.3 175.45 0 73.44.31 0 82.97 0 176zm176-80c-44.11 0-80 35.89-80 80 0 8.84-7.16 16-16 16s-16-7.16-16-16c0-61.76 50.24-112 112-112 8.84 0 16 7.16 16 16s-7.16 16-16 16z"></path></svg><!-- <i class="fa fa-lightbulb"></i> --> Clique nos títulos abaixo para mais informações</h6>
+                                </div>
+                                    <div class="accordion-item">
+                                        <h2 class="accordion-header" id="Item01">
+                                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                                            <strong>Política</strong>
+                                            </button>
+                                        </h2>
+                                        <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="Item01" data-bs-parent="#AccordionFase01" style="">
+                                            <div class="accordion-body">
+                                                <p class="Texto">- Momento eleitoral;</p>
+                                                <p class="Texto">- Tende a se confundir com o governo;</p>
+                                                <p class="Texto">- Relaciona-se, diretamente, com as ações governamentais;</p>
+                                                <p class="Texto">- <strong><i>Politcs</i></strong>,faz referência às atividades políticas: o uso de procedimentos diversos que expressam relações de poder e se destinam a alcançar ou produzir uma solução pacífica de conflitos relacionados a decisões públicas.</p>
+                                                <div class="row">
+                                                <div class="col-md-8"></div>
+                                                <div class="col-md-4">- Adaptado de RUA, 2014</div></div>
+
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="accordion-item Texto">
+                                        <h2 class="accordion-header" id="Item02">
+                                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                                            <strong>Política Pública</strong>
+                                            </button>
+                                        </h2>
+                                        <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="Item02" data-bs-parent="#AccordionFase01" style="">
+                                            <div class="accordion-body">
+                                                <p class="Texto">- Desenvolvida a partir do processo da política;</p>
+                                                <p class="Texto">- <strong><i>Policy</i></strong> formulação de propostas, tomada de decisões e sua implatação por organizações públicas, tendo como foco temas que afetam a coletividade, mobilizando interesses e conflitos;</p>
+                                                <p class="Texto">- Conjunto das decisões e ações relativas à alocação imperativa de valores envolvendo bens públicos;</p>
+                                                <p class="Texto">- Resultante da atividade política.</p>
+                                                <div class="row">
+                                                <div class="col-md-8"></div>
+                                                <div class="col-md-4">- Adaptado de RUA, 2014</div></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <br>
+
+                                <p class="Texto">Além da diferenciação entre os termos política (politics) e políticas públicas (polyces), Rua (2014) esclarece a respeito de dois outros termos que podem causar confusão e engano, vejamos.</p>
+
+                                <p class="Texto">Política pública é diferente de decisão política. Observemos os exemplos dados pela autora:</p>
+
+                                <div class="bd-callout bd-callout-success">
+                                    <p class="Texto">
+                                    <em>“Assim, embora uma política pública implique decisão política, nem toda decisão política chega a constituir uma política pública. Podemos encontrar um exemplo na emenda constitucional para reeleição presidencial, ou a CPMF, ou, ainda, a criação de um novo estado da federação. Observe que essas situações apresentadas são decisões, mas não são política pública. Já a reforma agrária, o sistema único de saúde, o financiamento da educação superior ou a adoção de mecanismos de transferência de renda são políticas públicas.”</em>
+                                    </p>
+
+                                    <p>– Rua, 2014</p>
+                                </div>
+                                <br>
+
+                                <p class="Texto">Política pública não é a mesma coisa que atividade coletiva. Rua (2014) nos ajuda compreender com mais um exemplo: </p>
+
+                                <div class="bd-callout bd-callout-success">
+                                    <p class="Texto">
+                                    <em>“O movimento pela cidadania contra a Fome e a miséria, o famoso “movimento do Betinho”, que deu ajuda a muitas famílias carentes era uma ação privada, ainda que tivesse interesse público. Já o Programa Bolsa Família é parte de uma política pública de combate à fome e à miséria. (...) a diferença entre eles é que o Programa Bolsa Família baseia-se em decisões e ações revestidas do poder extroverso e da autoridade soberana do Estado e por isso, consiste em uma política pública e não em uma ação privada de interesse público.”</em>
+                                    </p>
+
+                                    <p>– Rua, 2014</p>
+                                </div>
+                                <br>
+
+                                <p class="Texto">Buscamos assim o entendimento referente às diferenças entre política e política pública, preparando-nos para o próximo passo que será mergulhar na origem e nos conceitos, no ciclo e nas metodologias das políticas públicas. </p>
+                                <hr>
+
+                                <p class="Texto">A atenção sobre o tema política pública tem se evidenciado mais a partir das últimas décadas do século XX. Souza (2006) destaca três fatores para isso.</p>
+
+                                <p class="SubTitulo">Fatores que chamam atenção para o tema política pública</p>
+
+                                <p class="Texto">O primeiro deles está relacionado com as políticas restritivas de gastos, tendo em vista a mudança da compreensão do Estado intervencionista, fruto das políticas keynesianas, para a visão de um Estado mais restrito e com menos intervenções na economia e nas políticas sociais, visão está dada pela corrente conhecida como neoliberalismo.</p>
+
+                                <p class="Texto">O segundo fator, decorrente do primeiro, está relacionado com o surgimento de uma nova visão sobre o papel dos governos, produtores, por excelência, de políticas públicas.</p>
+
+                                <p class="Texto">O terceiro fator envolve a constatação da ineficácia dos governos de formar coalizões capazes de elaborar e implementar políticas públicas que impulsionam o desenvolvimento econômico, sem produzir exclusão social.</p>
+                                <hr>
+
+                                <p class="Texto">É nesse contexto que o campo da política pública, subárea da ciência Política, toma forma e se fortalece como área do conhecimento e disciplina acadêmica, inicialmente nos EUA como ferramenta de decisão do governo. Na figura seguinte, estão representados, segundo Souza (2006), os “pais” ou fundadores da área das políticas públicas. </p>
+
+                                <p class="Texto">Inexiste uma conceituação precisa, única e consensual do termo política pública, tendo em vista seu caráter multidisciplinar, que envolve várias disciplinas, teorias e modelos analíticos. O que encontramos são definições dadas por vários autores e que Souza (2006) resume como sendo política pública: </p>
+
+                                <div class="bd-callout bd-callout-success">
+                                    <p class="Texto">
+                                    <em>"O campo do conhecimento que busca, ao mesmo tempo, “colocar o governo em ação” e/ou analisar essa ação (variável independente) e, quando necessário, propor mudanças no rumo ou curso dessas ações (variável dependente). A formulação de políticas públicas constitui-se no estágio em que os governos democráticos traduzem seus propósitos e plataformas eleitorais em programas e ações que produzirão resultados ou mudanças no mundo real."</em>
+                                    </p>
+
+                                    <p>– Souza, 2006</p>
+                                </div>
+
+                                <p class="Texto">A mesma autora nos apresenta outras definições e seus respectivos formuladores, como exposto a seguir: </p>
+
+                                <p class="SubSubTitulo">O que são políticas públicas?</p>
+
+                                <ul>
+                                <li><p class="Texto">Um campo dentro do estudo da política que analisa o governo à luz de grandes questões públicas.</p></li>
+                                <li><p class="Texto">Um conjunto de ações do governo que irão produzir efeitos especificos.</p></li>
+                                <li><p class="Texto">É a soma das atividades dos governos, que agem diretamente ou através de delegação e que influenciam a vida dos cidadãos.</p></li>
+                                <li><p class="Texto">"O que o governo escolhe fazer ou não fazer".</p></li>
+                                <li><p class="Texto">Decisões e análises sobre política pública implicam responder às seguintes questões: quem ganha o quê, por quê e que diferença faz.</p></li>
+                                </ul>
+                                <br>
+
+                                <p class="Texto">Encerrando essa parte conceitual, é importante ressaltar que a fase de implementação da política pública é seguida por seu desdobramento em planos, programas, projetos, bases de dados ou pesquisas. Muitas vezes, se torna necessária a aprovação de uma nova legislação. A partir do momento de implementação das políticas públicas, é possível executar ações de monitoramento/acompanhamento e avaliação.</p>
+
+                                </div> <!-- CLASS PAGE TITLE FIM -->                            
+                        </div> <!-- End col-md-10 -->
+                        <div class="col-md-1"></div>
+                    </div> <!-- End row -->
+
+                    <!-- Botões de navegação -->
+                    <div class="text-center">
+                        <a href="#" class="btn btn-outline-success" role="button" aria-pressed="true"><i class="fa fa-chevron-left"></i> Voltar</a>
+                        <a href="#" class="btn btn-outline-success" role="button" aria-pressed="true">Próximo <i class="fa fa-chevron-right"></i></a>
+                    </div>
+
+                </div>
+            
+            <!-- FOOTER -->
+            <?php include('layout/footer.php') ?>
+        
+        </div> <!-- DIV MAIN FIM -->
+        
+        <!-- FOOTER JS -->
+        <?php include('layout/js.php') ?>
+
+</body>
+</html>
